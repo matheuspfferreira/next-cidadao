66aa61c4e6706317ce68578d6b1925c1d9e62138
Guilherme Augusto dos Santos <guilhermeaugusto923@gmail.com>
2026-04-08 00:22:13 -0300

    algumas mudanças no ServiceRequestService

--- src/main/java/br/com/demo/services/ServiceRequestService.java
+++ src/main/java/br/com/demo/services/ServiceRequestService.java
@@ -8,15 +8,12 @@ import org.json.simple.JSONObject;
 import java.util.List;
 
 public class ServiceRequestService {
-    // 1. Criamos um atributo para o repositório
     private final ServiceRequestRepository repository;
 
-    // 2. Criamos o CONSTRUTOR para aceitar o repositório (Resolve o erro do "repository" vermelho no Main)
     public ServiceRequestService(ServiceRequestRepository repository) {
         this.repository = repository;
     }
 
-    // 3. Criamos o método saveRequest (Resolve o erro do "saveRequest" vermelho no Main)
     public void saveRequest(ServiceRequestModel model) {
         try {
             this.repository.save(model);
@@ -25,12 +22,10 @@ public class ServiceRequestService {
         }
     }
 
-    // 4. Criamos o método getAllRequests (Resolve o erro do "getAllRequests" vermelho no Main)
     public List<JSONObject> getAllRequests() {
         return this.repository.getServiceRequests();
     }
 
-    // Método que você já tinha (removi o static para ele usar o repositório da instância)
     public Boolean sendServiceRequest(ServiceRequestType serviceRequestType) {
         return serviceRequestType.sendServiceRequest();
     }
